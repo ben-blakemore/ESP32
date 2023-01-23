fb8ed9ae2d7d3171afede8c1af82d9b197474bf7
ben-blakemore <blakemoreb1000@gmail.com>
2023-01-23 20:23:44 +0000

    started wifi tutorial

--- ESP-NOW/src/main.c
+++ ESP-NOW/src/main.c
@@ -1,5 +1,35 @@
+#include <stdio.h>
 #include<string.h>;
+#include<esp_now.h>;
+#include<esp_wifi.h>;
 
-void app_main() {
-char str[] = "40:22:D8:04:47:00";
+char board1MacAddress[] = "40:22:D8:04:47:00";
+char board2MacAddress[] = "E0:5A:1B:6C:2F:8C";
+
+void main() {
+    setup();
+}
+
+typedef struct struct_message {
+    char a[32];
+    int b;
+    float c;
+    bool d;
+} struct_message;
+
+// Create a structured object
+struct_message myData;
+ 
+// Peer info
+esp_now_peer_info_t peerInfo;
+ 
+// Callback function called when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  printf("\r\nLast Packet Send Status:\t");
+  printf(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+void setup() {
+    #define wifi_init_config_t config = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(wifi_init_config_t *config);
 }
